3ac5a5d76cc149a0912424b985f29265b8c97f63
Renato Cavalcante <renatocavalcante93@hotmail.com>
2017-02-17 22:23:46 -0300

    simulation bar using points

---
+++ point_bar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Renato
+#
+# 02/2017
+#
+# Point bar
+
+for i in $(seq 1 100)
+do
+    echo -n "Loading "
+
+    for j in $(seq 1 50)
+    do 
+        # j='.'
+        echo -n "."	# print on the same line.
+        sleep 0.0001
+    done
+    
+    sleep 0.1
+    echo -n " $i%"	# print on the same line.
+    echo -e		# new line.
+done
+
+echo -e "Finished"      # new line and print.
+echo -e			# new line.
+
