a6b6fe2bb250ccd193194b510e9dc2f3b34cf155
Renato Cavalcante <renatocavalcante93@hotmail.com>
2017-02-17 22:24:42 -0300

    simulation bar using characters

---
+++ character_bar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+vert='|'
+incDir='/'
+horiz='_'
+incEsq='\'
+
+echo -ne "\tLoading .................................... "
+
+for i in $(seq 1 100)
+do
+
+    for j in $vert $incDir $horiz $incEsq
+    do 
+        echo -ne '\033[53G'
+        echo -n "$j  $i%"
+        sleep 0.4
+    done
+
+done
+
+echo "Finished"
+echo -e
+
