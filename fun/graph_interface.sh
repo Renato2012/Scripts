164f4d8fe54678aa086737dfed3ca6e939487a97
Renato Cavalcante <renatocavalcante93@hotmail.com>
2017-02-18 01:38:38 -0300

    update file

--- fun/graph_interface.sh
+++ fun/graph_interface.sh
@@ -2,8 +2,10 @@
 
 # Renato
 
-file=graph_int.db
-path=~/$file
+file=graph_interface.db
+log=graph_interface.log
+#path=~/$file		# to save /home/user/
+path=$file
 touch $path
 
 cor=$(dialog --stdout --menu 'As cores:' 0 0 0 add 'Adiciona ao arquivo' list 'lista o arquivo' del 'Deleta o usuario' sort 'Lista em ordem alfabética')
@@ -12,21 +14,24 @@ if [ "$#" -eq 0 ]; then		# if qtd parameter == 0
 
   case $cor in
     add) dialog --inputbox 'Digite o nome : e-mail' 0 0  2>>$path
-  ;;
+    ;;
 
-    list) cat $path
-  ;;
+    list) 
+      tail -f "$path" > "$log" & 
 
-    del) dialog --inputbox 'Digite o nome:' 0 0  2>>guardaUser
-      sed 's/ *$//' guardaUser
-      echo $guardaUser >$path
-  ;;
+      dialog --title 'Listando conteúdo' --tailbox "$log" 0 0
+    ;;
+
+    del) dialog --inputbox 'Digite o nome:' 0 0  2>>$log
+      sed 's/ *$//' $log
+      echo $log >$path
+    ;;
 
     sort) sort --key=1 $path
-  ;;
+    ;;
 
     *) echo $cor
-  ;;
+    ;;
   esac
 
   exit 0
